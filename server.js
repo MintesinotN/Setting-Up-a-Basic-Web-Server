026416eb3470df567cf6202213f90a173ba3ec47
HP <mintesinotbekele1240@gmail.com>
2024-11-15 23:42:29 +0300

    Name Route

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req,res) => {
-    
+    if (req.url === '/' && req.method === 'GET'){
+        res.statusCode = 200;
+        res.end('InterTecHub');
+    }
+    else if (req.url === '/name' && req.method === 'GET'){
+        res.statusCode = 200;
+        res.end('FullName: Mintesniot Bekele Negash');
+    }
 });
 
 server.listen(port,() => {
