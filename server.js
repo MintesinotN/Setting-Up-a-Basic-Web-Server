abdc5a61306a8d38954b7cef5626873bd1b6478a
HP <mintesinotbekele1240@gmail.com>
2024-11-18 10:03:56 +0300

    some of the word correction

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req,res) => {
     }
     else if (req.url === '/name' && req.method === 'GET'){
         res.statusCode = 200;
-        res.end('FullName: Mintesniot Bekele Negash');
+        res.end('FullName: Mintesinot Bekele Negash');
     }
     else if (req.url === '/hobby' && req.method === 'GET'){
         res.statusCode = 200;
@@ -20,7 +20,7 @@ const server = http.createServer((req,res) => {
     }
     else if (req.url === '/dream' && req.method === 'GET'){
         res.statusCode = 200;
-        res.end('Dream: My Dream is to lead my self on my own route. And after finding my interest properly I want to become an expert in that field.');
+        res.end('Dream: My Dream is to lead myself on my own route. And after finding my interest properly I want to become an expert in that field.');
     }
     else{
         res.statusCode = 404;
